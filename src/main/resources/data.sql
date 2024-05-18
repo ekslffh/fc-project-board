13e1c97fa820e41819e120a7663037ca226ba680
ekslffh <ekslffh@gmail.com>
2024-05-18 21:52:23 +0900

    #18 - 유저 계정 domain/repo 추가
    * AuditingFields 는 직접 사용하지 않으므로 추상클래스로 변경
    * 모델링에 맞게 UserAccount domain, repo 추가
    * 임시 계정 데이터 삽입
    * 유저 계정 테스트 코드 작성

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,9 @@
+-- 테스트 계정
+-- TODO: 테스트용이지만 비밀번호가 노출된 데이터 세팅. 개선하는 것이 좋을 지 고민해 보자.
+insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
+    ('uno', 'asdf1234', 'Uno', 'uno@mail.com', 'I am Uno.', now(), 'uno', now(), 'uno')
+;
+
 -- 123개의 게시글
 insert into ARTICLE (title, content, hashtag, created_at, created_by, modified_at, modified_by) values ('Curabitur convallis.', 'Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.', 'Mauv', '2024-02-11 23:21:48', 'Alexia', '2024-02-22 17:41:37', 'Deedee');
 insert into ARTICLE (title, content, hashtag, created_at, created_by, modified_at, modified_by) values ('In congue.', 'Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.', 'Violet', '2023-11-03 16:17:50', 'Wilfred', '2023-05-16 05:04:47', 'Henryetta');
