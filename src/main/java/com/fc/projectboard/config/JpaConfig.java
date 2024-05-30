9dfe3fe356189c27c9743009b051754047d69db3
ekslffh <ekslffh@gmail.com>
2024-05-30 21:21:37 +0900

    #45 - 전체 코드 리뷰
    전체코드를 리뷰해보고 잘못된 코드를 수정하고 주석을 추가하였습니다.

--- src/main/java/com/fc/projectboard/config/JpaConfig.java
+++ src/main/java/com/fc/projectboard/config/JpaConfig.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 @Configuration
 public class JpaConfig {
 
+    // Spring Data JPA 는 해당 빈을 이용하여 생성자 및 수정자의 정보를 채운다.
     @Bean
     public AuditorAware<String> auditorAware() {
         return () -> Optional.of("uno"); // TODO: 스프링 시큐리티로 인증 기능을 붙이게 될때, 수정할 계획
